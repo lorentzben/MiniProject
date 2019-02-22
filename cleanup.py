07eb4715bf1009115217725cf067082eeb8d3224
Ben Lorentz <blorentz@luc.edu>
2019-02-22 16:44:03 +0000

    Added cleanup file and now downloads file if not there

---
+++ cleanup.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("rm -rf OptionA_Ben_Lorentz")
+
+print("All Clean")
--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 #Ben Lorentz 2.22.19
 import os
 import logging
+dir = os.getcwd()
 
+if(not os.path.exists("OptionA_Ben_Lorentz")):
+    os.system("mkdir OptionA_Ben_Lorentz")
+    os.chdir(dir+"/OptionA_Ben_Lorentz")
+else:
+    os.chdir(dir+"/OptionA_Ben_Lorentz")
+#Start Logging in the correct dir 
 logging.basicConfig(filename='OptionA.log',level=logging.DEBUG)
 
 #todo check that directory "depen" exists and has X Y and Z in it
-logging.info("Making directory and moving into it")
-if(not os.path.exists("OptionA_Ben_Lorentz")):
-    os.system("mkdir OptionA_Ben_Lorentz")
-os.system("cd OptionA_Ben_Lorentz")
 
 if(not os.path.isfile("SRR8185310.sra")):
     logging.info("The reads were not found so we are downloading them now")
     os.system("wget ftp://ftp.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR818/SRR8185310/SRR8185310.sra")
+
+print("ok")
